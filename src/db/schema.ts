7a42965f45133049b885696d94262a4b5d939ae8
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-12-23 19:16:52 -0500

    Created Prompt Schema with Relation to User
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,12 @@
-import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
+import {
+  integer,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  varchar,
+} from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
   id: serial('id').primaryKey(),
@@ -9,10 +17,17 @@ export const users = pgTable('users', {
   createdAt: timestamp('created_at').defaultNow().notNull(),
 });
 
-// export const conversation = pgTable('conversation', {
-//   id: serial('id').primaryKey(),
-//   userId: text('user_id').notNull(),
-//   prompt: text('prompt').notNull(),
-//   response: text('response').notNull(),
-//   createdAt: timestamp('created_at').defaultNow().notNull(),
-// });
+export const userRelations = relations(users, ({ many }) => ({
+  messages: many(prompt),
+}));
+
+export const prompt = pgTable('prompt', {
+  id: serial('id').primaryKey(),
+  authorId: integer('author_id')
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+  index: integer('index').notNull(),
+  role: varchar('prompt', { length: 10 }).notNull(),
+  content: text('response').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+});
