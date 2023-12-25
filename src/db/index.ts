c226daaadce5816973b669ec95515b0a0fd83e09
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-12-25 16:49:17 -0500

    Attempted to Fix DB Error of Too Many Clients
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,11 +3,23 @@ import { Client } from 'pg';
 
 import * as schema from '@/db/schema';
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-});
+declare global {
+  var drizzle: Client | undefined;
+}
 
-client.connect();
+const createNewClient = () => {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+  });
+  client.connect();
+  return client;
+};
+
+const client = globalThis.drizzle ? globalThis.drizzle : createNewClient();
+
+if (process.env.NODE_ENV === 'development') {
+  globalThis.drizzle = client;
+}
 
 // Can only use db in server components
 export const db = drizzle(client, { schema });
