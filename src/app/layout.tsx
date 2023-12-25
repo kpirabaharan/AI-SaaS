f1791ffc190bc4a7fd372ef45ece5a7f1fd9b5e2
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-12-25 17:52:21 -0500

    Replaced Normal Sonner with ShadCN Sonner
    No change lol.
    
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { Toaster } from 'sonner';
+import { Toaster } from "@/components/ui/sonner"
 
 import { cn } from '@/lib/utils';
 import './globals.css';
